2252a280eae4de6272fec757abf411f4c8977287
ION606 <itamarorenn@protonmail.com>
2022-04-24 13:51:11 -0400

    Removed the audio player for now

--- commands/playAudio.js
+++ commands/playAudio.js
@@ -6,15 +6,13 @@ const { VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice')
 module.exports = {
     name: "playaudio",
     execute(message, args, client) {
-            // message.channel.send("This command has not been set up yet\nSorry!");
-            // return;
-            const ytdl = require('ytdl-core-discord');
-            const url = args[0];
+            message.channel.send("This command has not been set up yet\nSorry!");
+            return;
+            // const ytdl = require('ytdl-core-discord');
+            // const url = args[0];
             
-            async function play(connection, url) {
-                connection.playOpusStream(await ytdl(url));
-            }
-        
-        
+            // async function play(connection, url) {
+            //     connection.playOpusStream(await ytdl(url));
+            // }
     }
 }
