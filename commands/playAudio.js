e19faf967db6b86fadf5515fbdd2644e1c5b5af5
ION606 <itamarorenn@protonmail.com>
2022-04-30 15:51:55 -0400

    Added the playaudio command

--- commands/playAudio.js
+++ commands/playAudio.js
@@ -1,18 +1,97 @@
 const pathToFfmpeg = require('ffmpeg-static');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioResource } = require('@discordjs/voice');
 const { generateDependencyReport } = require('@discordjs/voice');
-const { VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice');
+const { VoiceConnectionStatus, AudioPlayerStatus, createAudioPlayer, StreamType } = require('@discordjs/voice');
+const play = require('play-dl');
+
+// Leave here to be initialized at the program's start
+const player = createAudioPlayer();
+
+// Note: Unsure of what this does , but may be related to the play-dl lib (my notes are inconsistent)
+// play.authorization();
+
 
 module.exports = {
     name: "playaudio",
-    execute(message, args, client) {
-            message.channel.send("This command has not been set up yet\nSorry!");
-            return;
-            // const ytdl = require('ytdl-core-discord');
-            // const url = args[0];
+    async execute(message, args, bot) {
+            // message.channel.send("This command has not been set up yet\nSorry!");
+            // return;
+            if (args[0] == "play") {
+                if (args.length < 1) {
+                    message.reply("Please specify a function (play, pause, unpause or stop)");
+                    return;
+                } else if (args.length < 2) {
+                    message.reply("Please provide a song url");
+                    return;
+                }
+            }
+
+            /*
+            Re-introduce once the issue with ydtl-core is resolved (see
+            https://github.com/porridgewithraisins/jam-bot#known-bugs)
+            const stream = await ytdl(url, { filter: 'audioonly' });
+            */
+            const channel = bot.channels.cache.get("930148609406685227");
+            const connection = joinVoiceChannel({
+                channelId: channel.id,
+                guildId: channel.guild.id,
+                adapterCreator: channel.guild.voiceAdapterCreator,
+            });
             
-            // async function play(connection, url) {
-            //     connection.playOpusStream(await ytdl(url));
-            // }
+            connection.on(VoiceConnectionStatus.Ready, () => {
+                console.log('Connected to the voice channel!');
+            });
+            
+            if (args[0] == "play") {
+                let stream;
+                let info = "Playing __***";
+                let yt_info;
+                if (args[1].startsWith("https://")) {
+                    if (!args[1].startsWith("https://www.youtube.com/") &&
+                    !args[1].startsWith("https://music.youtube.com/")) {
+                        message.reply("This is not a valid YouTube URL");
+                        return;
+                    }
+                    yt_info = await play.video_info(args[1]);
+                    // let stream = await play.stream_from_info(yt_info)
+                    stream = await play.stream(args[1]);
+
+                    console.log("Playing from a URL!");
+                } else {
+                    yt_info = await play.search(args.slice(1).join(' '), {
+                        limit: 1
+                    });
+
+                    stream = await play.stream(yt_info[0].url);
+                    yt_info = await play.video_info(yt_info[0].url);
+                }
+
+                //Add the video info to the return message
+                info += yt_info.video_details.title + "***__\n";
+                info += "Check it out at " + yt_info.video_details.url + "\n";
+
+
+                let resource = createAudioResource(stream.stream, {
+                    inputType: stream.type
+                })
+
+                connection.subscribe(player);
+
+                let audio = "em.mp3";
+                // let resource = createAudioResource(join(__dirname, audio));
+                player.play(resource);
+    
+                player.on(AudioPlayerStatus.Playing, () => {
+                    console.log('The audio player has started playing!');
+                });
+                message.reply(info);
+            } else if (args[0] == "pause") {
+                player.pause();
+            } else if (args[0] == "unpause") {
+                player.unpause();
+            } else if (args[0] == "stop") {
+                player.stop();
+                connection.destroy();
+            }
     }
 }
--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const Discord = require('discord.js');
 const { token } = require('./commands/currency/config.json');
 
 
-const client = new Client({ 
+const bot = new Client({ 
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_VOICE_STATES
     ],
 });
 
@@ -16,11 +17,11 @@ const prefix = '/';
 
 const fs = require('fs');
 const { exit } = require('process');
-client.commands = new Discord.Collection();
+bot.commands = new Discord.Collection();
 
-client.commNames = new Discord.Collection();
+bot.commNames = new Discord.Collection();
 
-client.econ = new Discord.Collection();
+bot.econ = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
@@ -30,31 +31,31 @@ let i = 0;
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, command);
-    client.commNames.set(i, [command.name, command.description]);
+    bot.commands.set(command.name, command);
+    bot.commNames.set(i, [command.name, command.description]);
     i ++;
 }
 
 //ECON SECTION
-client.commands.set('ECON', require(`./commands/currency/app.js`));
+bot.commands.set('ECON', require(`./commands/currency/app.js`));
 const currency = new Discord.Collection();
 const { Users } = require('./commands/currency/dbObjects.js');
 i++;
 
-client.commNames.set('length', i);
+bot.commNames.set('length', i);
 
 
-client.on('ready', async () => {
-    // client.once('ready', async () => {
+bot.on('ready', async () => {
+    // bot.once('ready', async () => {
         const storedBalances = await Users.findAll();
         storedBalances.forEach(b => currency.set(b.user_id, b));
 
-        // console.log(`Logged in as ${client.user.tag}!`);
+        // console.log(`Logged in as ${bot.user.tag}!`);
     console.log('SLEEMER BOT ONLINE!!!!! OH MY GOD OH MY GOD!!!');
 });
 
 
-client.on('messageCreate', (message) => {
+bot.on('messageCreate', (message) => {
     //COMMAND AREA
     //Check if the prefix exists
     if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -66,38 +67,38 @@ client.on('messageCreate', (message) => {
     //Check if the user has sufficient permission
     //Performes the command
     switch(command) {
-        case 'test': client.commands.get('Hello World').execute(message, args);
+        case 'test': bot.commands.get('Hello World').execute(message, args);
         break;
 
-        case 'profile': client.commands.get('profile').execute(message, args, Discord);
+        case 'profile': bot.commands.get('profile').execute(message, args, Discord);
         break;
 
-        case 'links': client.commands.get('links').execute(message, args, Discord);
+        case 'links': bot.commands.get('links').execute(message, args, Discord);
         break;
 
-        case 'arrow': client.commands.get('arrow').execute(message, args, Discord);
+        case 'arrow': bot.commands.get('arrow').execute(message, args, Discord);
         break;
 
-        case 'playaudio': client.commands.get('playaudio').execute(message, args, client, Discord);
+        case 'playaudio': bot.commands.get('playaudio').execute(message, args, bot, Discord);
         break;
         
-        case 'quotes': client.commands.get('quotes').execute(message, args, Discord, Client);
+        case 'quotes': bot.commands.get('quotes').execute(message, args, Discord, Client);
         break;
 
-        case 'extracredit': client.commands.get('EC').execute(message);
+        case 'extracredit': bot.commands.get('EC').execute(message);
         break;
 
-        case 'scrape': client.commands.get('scraper').execute(message, args);
+        case 'scrape': bot.commands.get('scraper').execute(message, args);
         break;
 
-        case 'kareoke': client.commands.get('kareoke').execute(message, args);
+        case 'kareoke': bot.commands.get('kareoke').execute(message, args);
         break;
 
-        default: client.commands.get('ECON').execute(client, prefix, message, args, command, Users, currency);
+        default: bot.commands.get('ECON').execute(bot, prefix, message, args, command, Users, currency);
     }
 })
 
 
 
 //Last Line
-client.login(token);
+bot.login(token);
