fb87d33bd92ec23f3d9cd23c3f547fe0b85cf0ef
ION606 <itamarorenn@protonmail.com>
2022-07-21 14:22:31 +0300

    Added the circle outline PNG

--- commands/Selmer Specific/postProfile.js
+++ commands/Selmer Specific/postProfile.js
@@ -7,7 +7,7 @@ module.exports = {
         .setTitle('My professional resume')
         //.setURL('https://discordjs.guide/popular-topics/embeds.html#embed-preview')
         //.setDescription('My professional resume')
-        .setImage('https://github.com/ION606/selmerBot/blob/main/Sleemer_Bringsjorgend.png?raw=true')
+        .setImage('https://github.com/ION606/selmerBot/blob/main/assets/Sleemer_Bringsjorgend.png?raw=true')
         .addFields(
             {name: 'My Epithets:', value: "~~Pearls of Wisdom~~"},
             {name: '\t__Epithet 1__', value: "_Negative money is the best money_"},
